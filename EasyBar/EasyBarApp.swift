a23293531cc846ebaa7a2c498c497a1503ea4c82
felipedurante-fil <xfelipedurantex@gmail.com>
2026-03-30 14:01:55 -0300

    Initial Commit

---
+++ EasyBar/EasyBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBarApp.swift
+//  EasyBar
+//
+//  Created by Felipe Durante on 30/03/26.
+//
+
+import SwiftUI
+
+@main
+struct EasyBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
